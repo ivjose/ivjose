c4b2493992b5a79a7291b2392630254ca52d5369
outcrate <jsantos@outcrate.xyz>
2020-07-12 15:43:38 +0800

    Remove code splitting

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,12 +1,8 @@
 import React, { Suspense } from 'react';
-// import HeroLanding from './HeroLanding';
-// import WhatDo from './WhatDo';
+import HeroLanding from './HeroLanding';
+import WhatDo from './WhatDo';
 
 // import Testimonials from './Testimonials';
-
-const HeroLanding = React.lazy(() => import('./HeroLanding'));
-const WhatDo = React.lazy(() => import('./WhatDo'));
-
 const Home = () => (
   <article>
     <Suspense fallback={<div>Loading...</div>}>
