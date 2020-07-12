f25d88537f10ebcd6752bdc4319efcbec8afc666
outcrate <jsantos@outcrate.xyz>
2020-07-12 15:01:51 +0800

    Added lazy loading component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import HeroLanding from './HeroLanding';
-import WhatDo from './WhatDo';
+import React, { Suspense } from 'react';
+// import HeroLanding from './HeroLanding';
+// import WhatDo from './WhatDo';
+
 // import Testimonials from './Testimonials';
+
+const HeroLanding = React.lazy(() => import('./HeroLanding'));
+const WhatDo = React.lazy(() => import('./WhatDo'));
 
 const Home = () => (
   <article>
-    <HeroLanding />
-    <WhatDo />
-    {/* <Testimonials /> */}
+    <Suspense fallback={<div>Loading...</div>}>
+      <HeroLanding />
+      <WhatDo />
+      {/* <Testimonials /> */}
+    </Suspense>
   </article>
 );
 
