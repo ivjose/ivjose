adccc1801168cb73a81375d270659fe835cd63d5
outcrate <jsantos@outcrate.xyz>
2020-10-19 16:39:08 +0800

    Initial template for blog

---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { graphql } from 'gatsby';
+import Layout from 'components/layout';
+
+const Blog = () => {
+  const title = 'TASDASD';
+  return <Layout>{title}</Layout>;
+};
+
+export default Blog;
